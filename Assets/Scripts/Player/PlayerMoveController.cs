1060077720192544072b33f04adac40bbfaabbfd
ArHiFranki <franki550@gmail.com>
2022-05-27 15:16:57 +0700

    Добавил ускорение анимации передвижения при движении вперёд и её замедление при движении назад

--- Assets/Scripts/Player/PlayerMoveController.cs
+++ Assets/Scripts/Player/PlayerMoveController.cs
@@ -7,8 +7,15 @@ public class PlayerMoveController : MonoBehaviour
     [SerializeField] private float minHeight;
     [SerializeField] private float maxWidth;
     [SerializeField] private float minWidth;
+    [SerializeField] private float moveForwardAnimationSpeed = 1.5f;
+    [SerializeField] private float moveBackwardAnimationSpeed = 0.8f;
+    [SerializeField] private Animator playerAnimator;
 
     private Vector2 playerMoveInput;
+    private Vector3 deltaPosition;
+    private Vector3 oldPosition;
+    private Vector3 newPosition;
+    private const string moveAnimationSpeedValue = "moveSpeed";
 
     private void Update()
     {
@@ -22,12 +29,30 @@ public class PlayerMoveController : MonoBehaviour
 
     private void PlayerMove()
     {
-        Vector3 deltaPosition = playerMoveInput * moveSpeed * Time.deltaTime;
-        Vector3 newPosition = transform.position + deltaPosition;
+        oldPosition = transform.position;
+        deltaPosition = playerMoveInput * moveSpeed * Time.deltaTime;
+        newPosition = transform.position + deltaPosition;
 
         newPosition.x = Mathf.Clamp(newPosition.x, minWidth, maxWidth);
         newPosition.y = Mathf.Clamp(newPosition.y, minHeight, maxHeight);
 
+        SetMoveSpeedAnimation();
         transform.position = newPosition;
+    }
+
+    private void SetMoveSpeedAnimation()
+    {
+        if (newPosition.x < oldPosition.x)
+        {
+            playerAnimator.SetFloat(moveAnimationSpeedValue, moveBackwardAnimationSpeed);
+        }
+        else if (newPosition.x == oldPosition.x)
+        {
+            playerAnimator.SetFloat(moveAnimationSpeedValue, 1f);
+        }
+        else
+        {
+            playerAnimator.SetFloat(moveAnimationSpeedValue, moveForwardAnimationSpeed);
+        }
     }
 }

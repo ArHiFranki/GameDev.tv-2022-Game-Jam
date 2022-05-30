9671438f2d0b2628987ebdbdd0ba03ab20580254
ArHiFranki <franki550@gmail.com>
2022-05-30 20:59:29 +0700

    Добавил контроллер настроек

---
+++ Assets/Scripts/Controllers/SettingsController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour
+{
+    [SerializeField] private float musicVolume;
+    [SerializeField] private float effectsVolume;
+    [SerializeField] private float defaultMusicVolume;
+    [SerializeField] private float defaultEffectsVolume;
+
+    public float MusicVolume => musicVolume;
+    public float EffectsVolume => effectsVolume;
+
+    private void Awake()
+    {
+        int objectsCount = FindObjectsOfType<SettingsController>().Length;
+
+        if (objectsCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        musicVolume = value;
+    }
+
+    public void SetEffectsVolume(float value)
+    {
+        effectsVolume = value;
+    }
+}
